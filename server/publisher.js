b3bef689a91ae32b40750078fed0c10a45021a03
Chintan Zaveri <zaveri_chintan@rediff.com>
2016-05-03 17:31:45 +0530

    Slack clone

---
+++ server/publisher.js
@@ -0,0 +1,11 @@
+Meteor.publish('messages', function () {
+    return Messages.find();
+});
+
+
+Meteor.publish("allUsernames", function () {
+  return Meteor.users.find({}, {fields: {
+    "username": 1,
+    "services.github.username": 1
+  }});
+});
