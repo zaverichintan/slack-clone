16f1cb17dc013880d53b63932b53cb412b52d698
Chintan Zaveri <zaveri_chintan@rediff.com>
2016-05-03 23:53:18 +0530

    Security issues fix

--- client/app.js
+++ client/app.js
@@ -4,7 +4,11 @@ Template.messages.helpers({
 
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_AND_EMAIL'
-})
+});
+
+Template.registerHelper('currentChannel', function () {
+	return Session.get('channel');
+});
 
 Template.registerHelper("timestampToTime", function (timestamp) {
 	var date = new Date(timestamp);
@@ -25,5 +29,18 @@ Template.registerHelper("usernameFromId", function (userId) {
 	return user.username;
 });
 
-Meteor.subscribe('messages');
-Meteor.subscribe('allUsernames');
+Template.listings.helpers({
+	channels: function () {
+		return Channels.find();
+	}
+});
+
+Template.channel.helpers({
+	active: function () {
+		if (Session.get('channel') === this.name) {
+			return "active";
+		} else {
+			return "";
+		}
+	}
+});
